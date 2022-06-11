71066d4c2a7ba10a71f6f9b11e6e410f4b341bbc
FlutLab.io <>
2022-06-11 20:52:35 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-//import 'package:myflutterapp/splashScreen.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,7 +7,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      //home: SplashScreen(),
+      home: SplashScreen(),
     );
   }
 }
