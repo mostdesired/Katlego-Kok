87260d51c4603118256fe9a9693233260b94ab63
FlutLab.io <>
2022-06-11 06:58:17 +0000

    Autosave

---
+++ lib/splashscreen.dart
@@ -0,0 +1,55 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // Application name
+
+      // Application theme data, you can set the colors for the application as
+      // you want
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      // A widget which will be started on application startup
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class Splashscreen extends StatelessWidget {
+  const Splashscreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return animatedSplashScreen(
+        splash: Column(
+          children: [
+            Image.asset(asset / logo.png),
+          ],
+        ),
+        backgroundColor: Colors.yellow,
+        nextScreen: const Splashscreen());
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // The title text which will be shown on the action bar
+        title: Text(title),
+      ),
+    );
+  }
+}
