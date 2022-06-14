767a649d26c212e6514ec24db110bda27acd77e0
FlutLab.io <>
2022-06-14 15:51:15 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:myflutterapp/splashScreen.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  //const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
