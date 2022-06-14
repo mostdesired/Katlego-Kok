71db0e125a551b4d3f0fc1cc03031ac69a4ead2a
FlutLab.io <>
2022-06-14 14:51:33 +0000

    Autosave

--- lib/loginscreen.dart
+++ lib/loginscreen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'dashboard.dart';
 import 'splashScreen.dart';
 
-void main() => runApp(const MyApp());
+//void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
