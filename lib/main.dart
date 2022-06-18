3950c0605a70aea22c2d807c9b315ca778fe18ea
FlutLab.io <>
2022-06-18 23:19:14 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -8,26 +8,53 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      //home: SplashScreen(),
+      home: SplashScreen(),
     );
   }
 }
 
-@override
-Widget build(BuildContext context) {
-  return initWidget();
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => SplashScreenState();
 }
 
-initWidget() {
-  return Scaffold(
-      body: Stack(children: [
-    Container(
-      color: Colors.white,
-    ),
-    Center(
-      child: Container(
-        child: Image.asset("images/logo.jpg"),
+class SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    startTimer();
+  }
+
+  startTimer() async {
+    var duration = Duration(seconds: 3);
+    return startTimer();
+  }
+
+  loginRoute() {
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => MyStatefulWidget()),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return initWidget();
+  }
+
+  initWidget() {
+    return Scaffold(
+        body: Stack(children: [
+      Container(
+        color: Colors.white,
       ),
-    ),
-  ]));
+      Center(
+        child: Container(
+          child: Image.asset("images/logo.jpg"),
+        ),
+      ),
+    ]));
+  }
 }
