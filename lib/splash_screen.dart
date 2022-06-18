7778c5d87bfaeccbad18f8f3b04717e12231e331
FlutLab.io <>
2022-06-18 21:24:21 +0000

    Autosave

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'loginscreen.dart';
+//import 'loginscreen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
