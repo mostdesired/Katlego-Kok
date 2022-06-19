8a05343eea82474b80722b5a17a9d69f3fa18aba
FlutLab.io <>
2022-06-19 19:23:33 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class _MyAppState extends State<MyApp> {
   super.initState();
   new Future.delayed(const Duration(seconds: 3));
   () async => Navigator.push(context, MaterialPageRoute(builder: (context) => MyStatefulWidget()),
-  ));
+  ););
  }
 
   @override
