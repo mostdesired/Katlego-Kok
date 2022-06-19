b5714d50f727b4efc297655488de31783a321e8f
FlutLab.io <>
2022-06-19 19:35:31 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -5,38 +5,37 @@ import 'dart:async';
 
 void main() => runApp(MyApp());
 
- class MyApp extends StatefulWidget {
- @override
- _MyAppState createState() => new _MyAppState();
-
- }
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
 
 class _MyAppState extends State<MyApp> {
   @override
- void initState(){
-  super.initState();
-  new Future.delayed(const Duration(seconds: 3));
-  () async => Navigator.push(context, MaterialPageRoute(builder: (context) => MyStatefulWidget()),
-  ););
- }
+  void initState() {
+    super.initState();
+    new Future.delayed(const Duration(seconds: 3));
+    () async => Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => MyStatefulWidget();),
+        );
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
           body: Stack(children: [
-        Container(
-          color: Colors.white,
-        ),
-        Center(
-          child: Container(
-          child: Image.asset("images/logo.jpg"),
-        ),
-      )
-       ]),
-     )
-    );
+            Container(
+              color: Colors.white,
+            ),
+            Center(
+              child: Container(
+                child: Image.asset("images/logo.jpg"),
+              ),
+            )
+          ]),
+        ));
   }
 }
-
