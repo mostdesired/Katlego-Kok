aa13c6777b000268760e7365ee1a133046d675ae
FlutLab.io <>
2022-06-20 17:33:35 +0000

    Autosave

--- lib/loginscreen.dart
+++ lib/loginscreen.dart
@@ -27,72 +27,74 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(10),
-      child: ListView(
-        children: <Widget>[
-          Container(
-              alignment: Alignment.center,
+    return Container(
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: ListView(
+          children: <Widget>[
+            Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(10),
+                child: const Text(
+                  'Welcome',
+                  style: TextStyle(color: Colors.blue, fontWeight: FontWeight.w500, fontSize: 30),
+                )),
+            Row(
+              children: <Widget>[
+                const Text('Insert your login details below'),
+              ],
+              mainAxisAlignment: MainAxisAlignment.center,
+            ),
+            Container(
               padding: const EdgeInsets.all(10),
-              child: const Text(
-                'Welcome',
-                style: TextStyle(color: Colors.blue, fontWeight: FontWeight.w500, fontSize: 30),
-              )),
-          Row(
-            children: <Widget>[
-              const Text('Insert your login details below'),
-            ],
-            mainAxisAlignment: MainAxisAlignment.center,
-          ),
-          Container(
-            padding: const EdgeInsets.all(10),
-            child: TextField(
-              controller: nameController,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'UserName',
+              child: TextField(
+                controller: nameController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'UserName',
+                ),
               ),
             ),
-          ),
-          Container(
-            padding: const EdgeInsets.all(10),
-            child: TextField(
-              obscureText: true,
-              controller: passwordController,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Password',
+            Container(
+              padding: const EdgeInsets.all(10),
+              child: TextField(
+                obscureText: true,
+                controller: passwordController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Password',
+                ),
               ),
             ),
-          ),
-          TextButton(
-            onPressed: () {
-              //forgot password screen
-            },
-            child: const Text(
-              'Forgot Password',
-            ),
-          ),
-          Container(
-            height: 50,
-            padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-            child: ElevatedButton(
-              child: const Text('Login'),
+            TextButton(
               onPressed: () {
-                print(nameController.text);
-                print(passwordController.text);
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (BuildContext context) {
-                      return Dashpage();
-                    },
-                  ),
-                );
+                //forgot password screen
               },
+              child: const Text(
+                'Forgot Password',
+              ),
             ),
-          )
-        ],
+            Container(
+              height: 50,
+              padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+              child: ElevatedButton(
+                child: const Text('Login'),
+                onPressed: () {
+                  print(nameController.text);
+                  print(passwordController.text);
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (BuildContext context) {
+                        return Dashpage();
+                      },
+                    ),
+                  );
+                },
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
