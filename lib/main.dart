84f8e188184a4be62e36aabe936e4a45f5ad8edd
FlutLab.io <>
2022-06-20 16:12:06 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,29 @@
 //import 'package:flutter/cupertino.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'loginscreen.dart';
 import 'dart:async';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'flutter demo',
+      home: MyApplication(),
+    );
+  }
+}
+
+class MyApplication extends StatefulWidget {
+  const MyApplication({Key? key}) : super(key: key);
 
   @override
   _MyAppState createState() => new _MyAppState();
 }
 
-class _MyAppState extends State<MyApp> {
+class _MyAppState extends State<MyApplication> {
   @override
   void initState() {
     super.initState();
