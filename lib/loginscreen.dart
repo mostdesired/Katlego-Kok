40e5587a8af208b681306e6f5f58a505e044d18f
FlutLab.io <>
2022-06-20 17:12:42 +0000

    Autosave

--- lib/loginscreen.dart
+++ lib/loginscreen.dart
@@ -55,7 +55,7 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
             ),
           ),
           Container(
-            padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+            padding: const EdgeInsets.all(10),
             child: TextField(
               obscureText: true,
               controller: passwordController,
